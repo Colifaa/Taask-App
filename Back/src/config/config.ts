7d5c2b9ae9f5af1cc0f61f159288b766d0d55d88
unknown <Jorgemathez14@gmail.com>
2025-05-02 19:28:11 -0300

    fix: add FRONTEND_URL variable and update config.ts to read it for CORS configuration

--- Back/src/config/config.ts
+++ Back/src/config/config.ts
@@ -41,7 +41,7 @@ export const config: Config = {
     clientSecret: process.env.API_CLIENT_SECRET!
   },
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-client-id', 'x-client-secret']
   },
